2e709f0d679ec1c9a2bb959c470d089dccf1b893
CarlottoLeo <leo_carlotto@hotmail.com.br>
2018-10-21 11:36:24 -0200

    site

--- js/carlotto.js
+++ js/carlotto.js
@@ -1,11 +1,20 @@
 $("#calcular").on("click", function(){
   var valor = $("#valor").val();
   var parcela = $("#parcela").val();
-  var taxa = $("#taxa").val();
+  var taxa = 1.57;
   var prestacao = 0;
 
   prestacao = valor * Math.pow((1 + (taxa/100)), parcela);
 
-  $("#prestacao").val(prestacao/parcela);
+  if((prestacao/parcela) > 0){
+      $("#prestacaoSicoob").text("R$ " + (prestacao/parcela).toFixed(2));
+  }
 
+})
+
+$("#limpar").on("click", function(){
+  $("#valor").val("");
+  $("#parcela").val("");
+  $("#taxa").val("");
+  $("#prestacaoSicoob").text("");
 })
